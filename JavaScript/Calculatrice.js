26a6dda71605a453d09bc6dd0f3382954b0c1844
christophe maggiore <chrismag06000@gmail.com>
2024-10-03 14:49:36 +0200

    Ajout événement

--- JavaScript/Calculatrice.js
+++ JavaScript/Calculatrice.js
@@ -1,4 +1,52 @@
-function Calculatrice(){
+
+// const element = document.getElementById("demo");
+// element.addEventListener("click", myFunction);
+
+// function myFunction() {
+//   document.getElementById("Screen").innerHTML = "Hello World";
+// }
+
+console.log("bonjour .....")
+
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+const buttSept = document.getElementById("sept");
+
+const textButtCE = document.getElementById("CE").textContent;
+const textButtsupprimer = document.getElementById("supprimer").textContent;
+const textButtpourcent = document.getElementById("pourcent").textContent;
+const textButtaddition = document.getElementById("addition").textContent;
+const textButtSept = document.getElementById("sept").textContent;
+const textButthuit = document.getElementById("huit").textContent;
+const textButtneuf = document.getElementById("neuf").textContent;
+const textButtmoins = document.getElementById("moins").textContent;
+const textButtquatre = document.getElementById("quatre").textContent;
+const textButtcinq = document.getElementById("cinq").textContent;
+const textButtsix = document.getElementById("six").textContent;
+const textButtmultiple = document.getElementById("multiple").textContent;
+const textButtun = document.getElementById("un").textContent;
+const textButtdeux = document.getElementById("deux").textContent;
+const textButttrois = document.getElementById("trois").textContent;
+const textButtdivision= document.getElementById("division").textContent;
+const textButtzero = document.getElementById("zero").textContent;
+const textButtMoy = document.getElementById("Moy").textContent;
+const textButtvirgule = document.getElementById("virgule").textContent;
+const textButtegale = document.getElementById("egale").textContent;
+
+console.log(sept)
+
+sept.addEventListener("click", function(){
+    console.log("bonjour salima, abir, chris!", textButtSept);
+});
+
+function Somme(a){
+
 
     
 }
